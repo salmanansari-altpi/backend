0f418a0066e974335b870050e12d13a4c19cc1e8
= <=>
2024-09-03 17:52:03 +0530

    edhi

--- services/common.js
+++ services/common.js
@@ -7,7 +7,7 @@ let transporter = nodemailer.createTransport({
   secure: false, // true for 465, false for other ports
   auth: {
     user: "salmanansari.altpi@gmail.com", // gmail
-    pass: 'fzjq lzvl scnf ueti', // pass
+    pass: "fzjq lzvl scnf ueti", // pass
   },
 });
 
